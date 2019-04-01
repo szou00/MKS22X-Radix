fb853677060120dbea51f91e42d959e66fdde550
Sharon Zou <szou00@stuy.edu>
2019-04-01 10:59:55 -0400

    created buckets for all of them

--- Radix.java
+++ Radix.java
@@ -1,7 +1,41 @@
 public class Radix {
 
   public static void radixsort(int[]data){
-    
+    //sort the first digit
+
+    //create linkedlist for all of them?
+
+    //pos buckets
+    MyLinkedList<Integer> zero = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> one = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> two = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> three = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> four = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> five = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> six = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> seven = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> eight = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nine = new MyLinkedList<Integer>();
+
+    //neg buckets
+    MyLinkedList<Integer> nzero = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> none = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> ntwo = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nthree = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nfour = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nfive = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nsix = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nseven = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> neight = new MyLinkedList<Integer>();
+    MyLinkedList<Integer> nnine = new MyLinkedList<Integer>();
+
+    //going through the list and starting with
+    for (int i = 0; i < data.length; i++) {
+      
+    }
+
+    //
+
   }
 
 }
