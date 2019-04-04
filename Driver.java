3ea00230047b4ce2d703f69811e86251fca21eec
Sharon Zou <szou00@stuy.edu>
2019-04-04 05:18:46 -0400

    driver copied over as well

--- Driver.java
+++ Driver.java
@@ -1,17 +1,25 @@
 public class Driver {
 
   public static void main(String[] args) {
-    int[] test1 = {2,4,12,4,2,7};
-    int[] test2 = {-2,-4,-12,-4,-2,-7};
-    MyLinkedList<Integer> test1L = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> test2L = new MyLinkedList<Integer>();
-    for (int i = 0; i < test1.length; i++) {
-      test1L.add(test1[i]);
-    }
-    for (int i = 0; i < test2.length; i++) {
-      test2L.add(test2[i]);
-    }
-    System.out.println(test1L);
-    System.out.println(test2L);
+
+    //testing MyLinkedList
+    // int[] test1 = {2,4,12,4,2,7};
+    // int[] test2 = {-2,-4,-12,-4,-2,-7};
+    // MyLinkedList<Integer> test1L = new MyLinkedList<Integer>();
+    // MyLinkedList<Integer> test2L = new MyLinkedList<Integer>();
+    // for (int i = 0; i < test1.length; i++) {
+    //   test1L.add(test1[i]);
+    // }
+    // for (int i = 0; i < test2.length; i++) {
+    //   test2L.add(test2[i]);
+    // }
+    // System.out.println(test1L);
+    // System.out.println(test2L);
+
+
+    //testing Radix
+    Radix test3r = new Radix();
+    int[] test3 = {103,45,241,861,359,175,920,4};
+    test3r.radixsort(test3);
   }
 }
