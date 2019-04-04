0780594b8399f840316f93754a82b07b51bf7f3d
Sharon Zou <szou00@stuy.edu>
2019-04-04 05:08:18 -0400

    moved over all the things that I worked in class here (through filezilla)

--- Radix.java
+++ Radix.java
@@ -5,36 +5,55 @@ public class Radix {
 
     //create linkedlist for all of them?
 
-    //pos buckets
-    MyLinkedList<Integer> zero = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> one = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> two = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> three = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> four = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> five = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> six = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> seven = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> eight = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nine = new MyLinkedList<Integer>();
+    //make an array of mylinkedlist for the buckets
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
 
-    //neg buckets
-    MyLinkedList<Integer> nzero = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> none = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> ntwo = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nthree = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nfour = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nfive = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nsix = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nseven = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> neight = new MyLinkedList<Integer>();
-    MyLinkedList<Integer> nnine = new MyLinkedList<Integer>();
-
-    //going through the list and starting with
+    //go through the list and find max number
+    //FIND ABSOLUTE VALUE !!! REMEMBER
+    int max = data[0];
     for (int i = 0; i < data.length; i++) {
-      
+      if (data[i] > max) {
+        max = data[i];
+      }
+    }
+    // System.out.println(max);
+
+    //finds the number of digits in max
+    int md = 1; //including the digit that's below 10
+    while (max > 10) {
+      md++;
+      max=max/10;
+    }
+    // System.out.println(d);
+
+    //initializing all the buckets
+    for (int i = 0; i < buckets.length; i++) {
+      buckets[i] = new MyLinkedList<Integer>();
     }
 
-    //
+    //sorting out the numbers by digits and putting them into their respective buckets
+    //first copy into linkedlist
+    for (int x = 0; x < data.length; x++) {
+      int b = data[i]%10; //getting their first digit
+      buckets[b].add(data[i]); //adding it in to their respective buckets
+    }
+
+    //sorting out the rest of the array
+    int digits = 1; //we already printed the first digit
+    while (digits != md) {
+      for (int x = 0; x < data.length; x++) {
+        int b = data[i]%(10) //WORK ON THIS U WANT TO GET 10^X !! 
+        buckets[b].add(data[i]); //adding it in to their respective buckets
+      }
+    }
+
+    //trying to test out the buckets and printing them out
+    for (int i = 0; i < buckets.length; i++) {
+      if (buckets[i].size() != 0) {
+        System.out.println(buckets[i].toString());
+      }
+    }
 
   }
 
