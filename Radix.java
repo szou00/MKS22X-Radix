f132c925e17df0caacbcb8c52271869530066a25
Sharon Zou <szou00@stuy.edu>
2019-04-07 09:38:08 -0400

    removed unneeded commented out code

--- Radix.java
+++ Radix.java
@@ -74,34 +74,6 @@ public class Radix {
     }
     // System.out.println(sorted.size());
 
-
-    // //sorting out the rest of the array
-    // int digits = 1; //we already sorted the first digit
-    // while (digits != md) {
-    //   for (int x = 0; x < sorted.size(); x++) {
-    //     // System.out.println("size: " + buckets[19].size() + " x: " + x );
-    //     int d = digits; //to use to divide everything
-    //     int newA = sorted.get(x); //to store the values
-    //     // System.out.println("d: "+d);
-    //     while (d != 0) { //keep dividing while it's not zero
-    //       newA = newA/10;
-    //       d--; //subtract the total num of digits
-    //     }
-    //     int b = newA % 10; //take the last digit after dividing
-    //     // System.out.println(newA + " " + md);
-    //
-    //     if (b >= 0) {
-    //       buckets[b+10].add(sorted.get(x)); //adding ifor pos nums
-    //     }
-    //     else {
-    //       buckets[9-Math.abs(b)].add(sorted.get(x)); //adding for neg numbers
-    //     }
-    //   }
-    //
-    //   digits++; //increase digits
-    //   merge(buckets,sorted); //merge everything together
-    // }
-
     // trying to test out the buckets and printing them out
     // for (int i = 0; i < buckets.length; i++) {
     //   // if (buckets[i].size() != 0) {
@@ -115,27 +87,16 @@ public class Radix {
 
   }
 
-  //adding it the 20th linked list
+  //merging
   // in their respective positions
   public static void merge(MyLinkedList<Integer>[] buckets, MyLinkedList<Integer> sorted) {
 
     //removes all the old elements in the array
-    // sorted.clear();
-
     for (int x = 0; x < buckets.length; x++) { //going through the buckets
     //   // System.out.println("buckets " + x + " " + buckets[x].toString());
-    //   while (buckets[x].size() > 0) { //while there are elements in the bucket
-    //     int index = 0; //begin at the first element
-    //     Integer value = buckets[x].get(index); //
-    //     sorted.add(value);
-    //     index++;
-    //     buckets[x].remove(value);
       if (buckets[x].size() > 0) {
         sorted.extend(buckets[x]);
-
       }
-            // }
-
     }
 
   }
